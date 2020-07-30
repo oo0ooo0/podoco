fa3b39424d3dc9cd58252f07418b792012c246e0
jamin-coo <jamin@cooljamm.com>
2020-07-31 02:56:32 +0900

    test: font styl in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Footer() {
-  return (
-    <footer>
-      <h1> h1 footer 푸터 1234</h1>
-      <h2> h2 footer 푸터 1234</h2>
-      <h3> h3 footer 푸터 1234</h3>
-      <h4> h4 footer 푸터 1234</h4>
-      <span> span footer 푸터 1234</span>
-      <p> p footer 푸터 1234</p>
-    </footer>
-  );
+  return <footer>footer</footer>;
 }
 export default Footer;
