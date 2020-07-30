4169fe191104ce99b8686bee82e66c9514b9fbb9
jamin-coo <jamin@cooljamm.com>
2020-07-31 03:01:21 +0900

    feat: add section1.2 to app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import Header from './Header';
 import Footer from './Footer';
+import Section1 from './Section1';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
-      hi 하이 22
+      <Section1 />
       <Footer />
     </>
   );
 }
-
+//
 export default App;
+
+//
