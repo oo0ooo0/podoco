e820dfdfb37b8c4445b7caa53894b68040a59a26
jamin-coo <jamin@cooljamm.com>
2020-07-31 02:58:13 +0900

    build: section2

---
+++ src/Section2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleSection2 = styled.header``;
+function Section2() {
+  return;
+  <StyledSection2></StyledSection2>;
+}
+export default Section2;
