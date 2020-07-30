1d01c5c8e698cee0dbbe8f25e049bd5ddcc670da
jamin-coo <jamin@cooljamm.com>
2020-07-30 23:40:59 +0900

    feat: styled-components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
 import Header from './Header';
 import Footer from './Footer';
 
 function App() {
   return (
-    < >
-             <Header />
-     hi
-             <Footer />
-
+    <>
+      <GlobalStyle />
+      <Header />
+      hi 하이 22
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
