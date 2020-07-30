407086b68c5315afaed150b4c5ce9d4dd1a9b61a
oo0ooo0 <nyanyanyangnyanga@gmail.com>
2020-07-30 13:08:04 +0900

    build: header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    < >
+             <Header />
      hi
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
