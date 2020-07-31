c9ab1e63ea5a4949a89293ade25f875693c97914
jamin-coo <jamin@cooljamm.com>
2020-08-01 04:43:00 +0900

    fix: moved hader into sec1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
-import Header from './Header';
 import Footer from './Footer';
 import Section1 from './Section1';
 import Section2 from './Section2';
+import Section3 from './Section3';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
       <Section1 />
       <Section2 />
       <Footer />
