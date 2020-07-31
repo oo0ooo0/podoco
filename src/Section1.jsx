fb2a037dae962a4c3684e983ccee4293b9a14a34
jamin-coo <jamin@cooljamm.com>
2020-08-01 04:38:10 +0900

    fix: sec1 style

--- src/Section1.jsx
+++ src/Section1.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import HeaderM from './HeaderM';
+import { Sec1Container } from './components/Containers';
 
 // const Logo = require('../public/img/logo.png');
 // const HeaderImg2 = require('../public/img/logo-img.png');
@@ -8,54 +10,40 @@ import styled from 'styled-components';
 // const HeaderImg4 = require('../public/img/zoom.png');
 
 const StyledSection1 = styled.section`
+  width: 720px;
   margin: 0 auto;
-  padding: 100px 30px;
+  margin-top: 45px;
   display: flex;
   flex-direction: column;
-
   justify-content: space-between;
   align-items: center;
-  h1 {
-    width: 200px;
-    background: rgba(0, 0, 0, 0.1);
-    img {
-      width: 100%;
-      background: rgba(0, 0, 0, 0.1);
-    }
+  img {
   }
-  nav {
-    ul {
-      display: flex;
-      li {
-        font: normal 400 18px/1 'Nanum Gothic';
-
-        margin: 0 20px;
-        background: rgba(0, 0, 0, 0.1);
-        vertical-align: middle;
-
-        img {
-          margin: 0 auto;
-          width: 50%;
-          background: rgba(0, 0, 0, 0.1);
-        }
-      }
-    }
+  h2 {
+    margin-top: 73px;
+  }
+  p {
+    margin: 35px 0 78px 0;
+    font: normal regular 26px/1 Nanum Gothic;
+    color: #363139;
+    opacity: 70%;
   }
 `;
 
 function Section1() {
   return (
-    <StyledSection1>
-      <img src='./img/logo-v.png' alt='PODO_REVIEW' />
-      <h2>
-        빅데이터 기반 리뷰 전문 미디어 
-      </h2>
+    <Sec1Container>
+      <HeaderM />
+      <StyledSection1>
+        <img src='./img/logo-v.png' alt='PODO_REVIEW' />
+        <h2>빅데이터 기반 리뷰 전문 미디어</h2>
 
-      <p>
-      포도리뷰에서는 현재 <em>293</em>개의 제품에 대한 <br/>
-      <em>384,719,481</em>개의 의견이 분석되고 있습니다      </p>
-
-    </StyledSection1>
+        <p>
+          포도리뷰에서는 현재 <em>293</em>개의 제품에 대한 <br />
+          <em>384,719,481</em>개의 의견이 분석되고 있습니다{' '}
+        </p>
+      </StyledSection1>
+    </Sec1Container>
   );
 }
 export default Section1;
