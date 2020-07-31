9a370a4614e6199e5f07a2291bb14b0f67982d33
jamin-coo <jamin@cooljamm.com>
2020-08-01 04:44:44 +0900

    build: container for sec1(header)

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
-export const LayoutContainer = styled.div`
-  width: ${(props) => props.width || 92}%;
-  margin: 10px auto;
+export const Sec1Container = styled.div`
+  /* width: ${(props) => props.width || 92}%; */
+  width:720px;
+  margin: 0 auto;  
   position: relative;
+  background: no-repeat url('./img/section1-bg.png');
+
 `;
 
-export const MainContainer = styled(LayoutContainer)`
-  margin-top: 230px;
-  @media (max-width: 768px) {
-    margin-top: 230px;
-  }
-`;
+// export const MainContainer = styled(LayoutContainer)`
+//   margin-top: 230px;
+//   @media (max-width: 768px) {
+//     margin-top: 230px;
+//   }
+// `;
