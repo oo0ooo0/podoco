89b6aaa9a3edccf92d34a41c58b6f822557ed33f
jamin-coo <jamin@cooljamm.com>
2020-08-01 22:58:28 +0900

    fix: changed style for responsive ver

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -7,16 +7,7 @@ const StyledSection2 = styled.section`
   position: relative;
   max-width: 1024px;
   margin: 0 auto;
-  :after {
-    content: '';
-    position: absolute;
-    right: 0; 
-    width: 100%;
-    bottom: 1px;
-    z-index: -1;
-    transform: scale(0.98);
-    box-shadow: 0px 0px 5px 2px #aaa;
-  }
+
   div {
     text-align: center;
     h2 {
@@ -85,65 +76,65 @@ const StyledSection2 = styled.section`
     }
   }
 
-  @media (min-width: 720px) {
+  @media (max-width: 720px) {
     div {
-      text-align: center;
       h2 {
+        padding-bottom: 1vw;
+        font-size: 5vw;
       }
     }
     img {
-      padding-top: 54px;
-      width: 30vw;
-      max-width: 300px;
+      padding-top: 6vw;
+      width: 60vw;
+      /* max-width: 60vw; */
       /* border: 1px solid red; */
     }
     .wrap {
-      margin: 0 auto;
-      width: 60%;
       .textWrap {
         h3 {
-          padding-top: 15px;
-          font: 800 30px/1 AppleGothic;
-          color: #8207c1;
+          padding-top: 1.5vw;
+          font-size: 4vw;
         }
         h4 {
-          padding-top: 25px;
-          font: 800 20px/1.2 Nanum Gothic;
-        }
+          padding-top: 2vw;
+          font-size: 5vw;
+        } 
         p {
-          margin-top: 13px;
-          font: 600 20px/1.2 Nanum Gothic;
-          color: #9c9a9e;
+          margin-top: 1.3vw;
+          font-size: 4vw;
         }
       }
-      .graphWrap {
-        margin: 0 27px;
-        .pie-chart2 {
-          display: inline-block;
-          position: relative;
-          width: 100px;
-          height: 100px;
-          background: conic-gradient(#00b953 75% 0%, #dde2df 0% 75%);
-          border-radius: 50%;
-        }
-        .pie-chart2 .center {
-          position: absolute;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 80px;
-          height: 80px;
-          background: #fff;
-          border-radius: 50%;
-          top: 10px;
-          left: 10px;
-          span {
-            font: italic 700 22px/1 Nanum Gothic;
-            color: #00b953;
+        .graphWrap {
+          margin: 0 3vw;
+          .pie-chart2 {
+            width: 16vw;
+            height: 16vw;
+          }
+          .pie-chart2 .center {
+            width: 13vw;
+            height: 13vw;
+            top: 1.5vw;
+            left: 1.5vw;
+            span {
+              font: italic 700 3vw/1 Nanum Gothic;
+            }
           }
         }
       }
     }
+  }
+`;
+
+const BoxShadow = styled.div`
+  :after {
+    content: '';
+    position: absolute;
+    right: 0;
+    width: 100%;
+    bottom: 1px;
+    z-index: -1;
+    transform: scale(0.98);
+    box-shadow: 0px 0px 5px 2px #aaa;
   }
 `;
 
@@ -155,34 +146,36 @@ const FlexBox = styled.div`
 
 function Section2() {
   return (
-    <Sec2Container>
-      <StyledSection2>
-        <div>
-          <h2>BEST PRODUCT</h2>
-        </div>
-        <FlexBox>
-          <img src='./img/best-product.png' alt='PODO_REVIEW' />
-          <div className='wrap'>
-            <div className='textWrap'>
-              <h3>이어폰 </h3>
-              <h4>
-                쿠거 COUGAR 200K Rainbow <br />
-                USB Red color
-              </h4>
-              <p>쿠거</p>
-            </div>
-            <div className='graphWrap'>
-              <div className='pie-chart2'>
-                <span className='center'>
-                  <span>75%</span>
-                </span>
+    <>
+      <Sec2Container>
+        <StyledSection2>
+          <div>
+            <h2>BEST PRODUCT</h2>
+          </div>
+          <FlexBox>
+            <img src='./img/best-product.png' alt='PODO_REVIEW' />
+            <div className='wrap'>
+              <div className='textWrap'>
+                <h3>이어폰 </h3>
+                <h4>
+                  쿠거 COUGAR 200K Rainbow <br />
+                  USB Red color
+                </h4>
+                <p>쿠거</p>
+              </div>
+              <div className='graphWrap'>
+                <div className='pie-chart2'>
+                  <span className='center'>
+                    <span>75%</span>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </FlexBox>
-        <div id='box' className='box-shadow'></div>
-      </StyledSection2>
-    </Sec2Container>
+          </FlexBox>
+        </StyledSection2>
+        <BoxShadow />
+      </Sec2Container>
+    </>
   );
 }
 export default Section2;
