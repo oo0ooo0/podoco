bd38f8e0f1c268060f04985246121189fd7954a4
jamin-coo <jamin@cooljamm.com>
2020-08-02 11:57:19 +0900

    fix: change style ( text-size & margin)

--- src/Section2.jsx
+++ src/Section2.jsx
@@ -34,16 +34,16 @@ const StyledSection2 = styled.section`
       text-align: left;
       h3 {
         padding-top: 15px;
-        font: 800 36px/1 AppleGothic;
+        font: 800 25px/1 Nanum Gothic;
         color: #8207c1;
       }
       h4 {
-        padding-top: 35px;
-        font: 800 27px/1.2 Nanum Gothic;
+        padding-top: 8px;
+        font: 800 35px/1.2 Nanum Gothic;
       }
       p {
-        margin: 13px 0 38px 0;
-        font: 600 24px/1.2 Nanum Gothic;
+        margin: 15px 0 38px 0;
+        font: 600 25px/1.2 Nanum Gothic;
         color: #9c9a9e;
       }
     }
@@ -77,79 +77,81 @@ const StyledSection2 = styled.section`
   }
   @media (min-width: 720px) {
     div {
-    text-align: center;
-    h2 {
-      margin: 0 auto;
-      display: inline-block;
-      padding-bottom: 20px;
-      border-bottom: 5px solid black;
-      color: #8804cb;
-    }
-  }
-  img {
-    margin: 0 auto;
-    padding-top: 54px;
-    width: 40vw;
-    max-width: 390px;
-    /* border: 1px solid red; */
-  }
-  .wrap {
-    width: 80%;
-    margin: 0 auto;
-    .textWrap {
-      text-align: left;
-      h3 {
-        font: 800 30px/1 AppleGothic;
-      }
-      h4 {
-        font: 800 27px/1.2 Nanum Gothic;
-      }
-      p {
-        font: 600 24px/1.2 Nanum Gothic;
-      }
-    }
-  }
-  @media (max-width: 700px) {
-    div {
+      text-align: center;
       h2 {
-        padding-bottom: 1vw;
-        font-size: 8vw;
+        margin: 0 auto;
+        display: inline-block;
+        padding-bottom: 20px;
+        border-bottom: 5px solid black;
+        color: #8804cb;
       }
     }
     img {
-      padding-top: 6vw;
-      width: 60vw;
-      /* max-width: 60vw; */
+      margin: 0 auto;
+      padding-top: 54px;
+      width: 40vw;
+      max-width: 390px;
       /* border: 1px solid red; */
     }
     .wrap {
+      width: 80%;
+      margin: 0 auto;
       .textWrap {
+        text-align: left;
         h3 {
-          padding-top: 1.5vw;
-          font-size: 4vw;
+          font: 800 30px/1 AppleGothic;
         }
         h4 {
-          padding-top: 2vw;
-          font-size: 5vw;
+          font: 800 27px/1.2 Nanum Gothic;
         }
         p {
-          margin-top: 1.3vw;
-          font-size: 4vw;
+          font: 600 24px/1.2 Nanum Gothic;
         }
       }
-      .graphWrap {
-        margin: 0 3vw;
-        .pie-chart2 {
-          width: 16vw;
-          height: 16vw;
+    }
+  }
+    @media (max-width: 500px) {
+      div {
+        h2 {
+          padding-bottom: 1vw;
+          font-size: 8vw;
         }
-        .pie-chart2 .center {
-          width: 13vw;
-          height: 13vw;
-          top: 1.5vw;
-          left: 1.5vw;
-          span {
-            font: italic 700 3vw/1 Nanum Gothic;
+      }
+      img {
+        padding-top: 6vw;
+        width: 60vw;
+        /* max-width: 60vw; */
+        /* border: 1px solid red; */
+      }
+      .wrap {
+        .textWrap {
+          h3 {
+            padding-top: 1.5vw;
+            font-size: 3.5vw;
+          }
+          h4 {
+            padding-top: 1vw;
+            font-size: 4vw;
+          }
+          p {
+            margin-top: 1.3vw;
+            font-size: 3.5vw;
+          }
+        }
+        .graphWrap {
+          margin: 0 3vw;
+          .pie-chart2 {
+            width: 16vw;
+            height: 16vw;
+          }
+          .pie-chart2 .center {
+            width: 13vw;
+            height: 13vw;
+            top: 1.5vw;
+            left: 1.5vw;
+            span {
+              font: italic 700 3vw/1 Nanum Gothic;
+            }
           }
         }
       }
